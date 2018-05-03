104993ac3a957394f3b315a5d7d5e7709435c78b
Michael Whyte <info@mwhyte.ca>
2018-05-03 14:58:01 -0700

    Add Autoprefixer tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const useref = require('gulp-useref');
 const uglify = require('gulp-uglify');
+const composer = require('gulp-uglify/composer');
+const uglifyES = require('uglify-es');
+const postcss = require('gulp-postcss');
 const gulpIf = require('gulp-if');
-const cssNano = require('gulp-cssnano');
 const htmlMin = require('gulp-htmlmin');
+const cssnano = require('cssnano');
 const imageMin = require('gulp-imagemin');
 const cache = require('gulp-cache');
 const del = require('del');
@@ -14,6 +18,8 @@ const runSequence = require('run-sequence');
 const rev = require('gulp-rev');
 const revReplace = require('gulp-rev-replace');
 const filter = require('gulp-filter');
+
+const minify = composer(uglifyES);
 
 // Intro Hello Task...
 gulp.task('hello', function() {
@@ -32,9 +38,31 @@ gulp.task('sass', function(){
 		}));
 });
 
+// Autoprefixer Task
+gulp.task('autoprefixer', function() {
+    return gulp.src('dev/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(postcss([autoprefixer({browsers: ['ie 8-11','last 2 versions']})]))
+        .pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('dev/styles'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
 // Watch Task
 gulp.task('watch', function(){
 	gulp.watch('dev/scss/**/*.scss', ['sass']);
+	gulp.watch('dev/*.html', browserSync.reload);
+	gulp.watch('dev/scripts/**/*.js', browserSync.reload);
+});
+
+// Watch Task with Autoprefixer
+gulp.task('watch-autoprefixer', function(){
+	gulp.watch('dev/scss/**/*.scss', ['autoprefixer']);
 	gulp.watch('dev/*.html', browserSync.reload);
 	gulp.watch('dev/scripts/**/*.js', browserSync.reload);
 });
@@ -48,21 +76,22 @@ gulp.task('browserSync', function(){
 	});
 });
 
-// Useref Task
-gulp.task('useref', function(){
-
-	const f = filter(['**/*.css', '**/*.js'], {restore: true});
+// Compress Task
+gulp.task('compress', function(){
+   
+    const f = filter(['**/*.css', '**/*.js'], {restore: true});
 
 	return gulp.src('dev/*.html')
 		.pipe(useref())
-		.pipe(gulpIf('*.js', uglify()))
-		.pipe(gulpIf('*.css', cssNano()))
+		.pipe(gulpIf('*.js', minify()))
+		.pipe(gulpIf('*.css', postcss([cssnano()])))
 		.pipe(gulpIf('*.html', htmlMin({collapseWhitespace: true})))
 		.pipe(f)
 		.pipe(rev())
 		.pipe(f.restore)
 		.pipe(revReplace())
 		.pipe(gulp.dest('dist'));
+
 });
 
 // Image Optimization Task
@@ -90,9 +119,14 @@ gulp.task('default', function() {
   runSequence(['sass', 'browserSync'], 'watch');
 });
 
+// Default Task with Autoprefixer
+gulp.task('default:autoprefixer', function() {
+	runSequence(['autoprefixer', 'browserSync'], 'watch-autoprefixer');
+});
+
 // Build Task
 gulp.task('build', function(){
-	runSequence('clean:dist', 'sass', ['useref', 'images', 'fonts']);
+	runSequence('clean:dist', 'autoprefixer', ['compress', 'images', 'fonts']);
 });
 
 
