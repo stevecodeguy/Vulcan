e703f695c5078197917bbf9c4978d305cd24da0e
steve990 <steve_990@hotmail.com>
2019-07-10 16:58:15 -0700

    Center bar of mobile nav fades on click

--- dev/scripts/nav.js
+++ dev/scripts/nav.js
@@ -1,2 +1,12 @@
-const hamburger = document.getElementById('menu');
+const hamburger = document.getElementsByClassName('menu')[0];
 
+function openClose() {
+    if (hamburger.classList.contains('close-menu') || hamburger.classList.contains('open-menu')) {
+        hamburger.classList.toggle('close-menu');
+        hamburger.classList.toggle('open-menu');
+    } else {
+        hamburger.classList.add('open-menu');
+    }
+}
+
+hamburger.addEventListener('click', openClose);
